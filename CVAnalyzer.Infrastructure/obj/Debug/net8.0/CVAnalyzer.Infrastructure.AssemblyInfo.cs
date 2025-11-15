ae97e29cac52816ff6f48c65e89bc13e8dbdf030
Binisha Aryal <I43991@verisk.com>
2025-11-15 22:24:47 +0545

    stable fixes

--- CVAnalyzer.Infrastructure/obj/Debug/net8.0/CVAnalyzer.Infrastructure.AssemblyInfo.cs
+++ CVAnalyzer.Infrastructure/obj/Debug/net8.0/CVAnalyzer.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CVAnalyzer.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43a0e6ebb69a3e984b9df80942fc16b728af1eb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f793519825f4fc0c57c09df29d6b8f7545792705")]
 [assembly: System.Reflection.AssemblyProductAttribute("CVAnalyzer.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CVAnalyzer.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
