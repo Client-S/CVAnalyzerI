f91fca78a0d8b61b87f600b4831dfc7846108487
Binisha Aryal <I43991@verisk.com>
2025-11-16 12:25:26 +0545

    initial commit

--- CVAnalyzer.Web/obj/Debug/net8.0/CVAnalyzer.Web.AssemblyInfo.cs
+++ CVAnalyzer.Web/obj/Debug/net8.0/CVAnalyzer.Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CVAnalyzer.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6887971021a52a5c51d167c997654024f413c18")]
 [assembly: System.Reflection.AssemblyProductAttribute("CVAnalyzer.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CVAnalyzer.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
